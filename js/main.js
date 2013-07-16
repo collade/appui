73f4e81cfc5a373bc5615d52d41dbc50a41479a9
Long Doan <longprao@gmail.com>
2013-07-17 00:28:23 +0700

    * done left sidebar

--- js/main.js
+++ js/main.js
@@ -12,14 +12,4 @@ angular.module('Collade', ['ui.bootstrap.collapse', 'ui.state', 'LoadingIndicato
     run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
         $rootScope.$state = $state;
         $rootScope.$stateParams = $stateParams;
-
-        if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
-            var viewportmeta = document.querySelector('meta[name="viewport"]');
-            if (viewportmeta) {
-                viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
-                document.body.addEventListener('gesturestart', function () {
-                    viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
-                }, false);
-            }
-        }
     }]);
