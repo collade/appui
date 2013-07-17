4208e7dcf2d69e0f003a74a779883f876de2a536
Long Doan <longprao@gmail.com>
2013-07-17 16:34:25 +0700

    * done categories

--- js/controllers.js
+++ js/controllers.js
@@ -2,5 +2,92 @@
 
 angular.module('Collade').
     controller('MainCtrl', ['$scope', function ($scope) {
+        $scope.data = {
+            categories: [
+                {
+                    "name": "Collade",
+                    "collapse": false,
+                    "children": [
+                        {
+                            "name": "#Design",
+                            "count": "1/4",
+                            "collapse": false,
+                            "children": [
+                                {
+                                    "name": "#Logo Design"
+                                },
+                                {
+                                    "name": "#Landing Page Design"
+                                },
+                                {
+                                    "name": "#App Design"
+                                },
+                                {
+                                    "name": "#Presentation Design"
+                                }
+                            ]
+                        },
+                        {
+                            "name": "#Development",
+                            "count": "5/18",
+                            "collapse": true,
+                            "children": [
+                                {
+                                    "name": "#Logo Design"
+                                },
+                                {
+                                    "name": "#Landing Page Design"
+                                },
+                                {
+                                    "name": "#App Design"
+                                },
+                                {
+                                    "name": "#Presentation Design"
+                                }
+                            ]
+                        },
+                        {
+                            "name": "#Marketing",
+                            "count": "9813",
+                            "collapse": true,
+                            "children": [
+                                {
+                                    "name": "#Logo Design"
+                                },
+                                {
+                                    "name": "#Landing Page Design"
+                                },
+                                {
+                                    "name": "#App Design"
+                                },
+                                {
+                                    "name": "#Presentation Design"
+                                }
+                            ]
+                        },
+                        {
+                            "name": "#Finance",
+                            "count": "2/5",
+                            "collapse": true,
+                            "children": [
+                                {
+                                    "name": "#Logo Design"
+                                },
+                                {
+                                    "name": "#Landing Page Design"
+                                },
+                                {
+                                    "name": "#App Design"
+                                },
+                                {
+                                    "name": "#Presentation Design"
+                                }
+                            ]
+                        }
 
+                    ]
+
+                }
+            ]
+        }
     }]);
